a55cf6871773ad88a83638ba9846f7ed639761b3
winimk <winimuthia7@gmail.com>
2025-09-28 15:00:45 +0700

    Add Timeout

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
-    pageLoadTimeout: 240000,
+    defaultCommandTimeout: 10000, // default 4000 ms → jadi 10 detik
+    pageLoadTimeout: 60000, // untuk tunggu load page
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/loginQA.cy.js
+++ cypress/e2e/loginQA.cy.js
@@ -5,68 +5,93 @@ describe("Scenario Verifikasi Fungsi Login", () => {
   });
 
   it("TC_001 - User login menggunakan username benar dan password benar", () => {
-    cy.get('input[name="username"]').type("Admin").should("have.value", "Admin");
-    cy.get('input[name="password"]').type("admin123").should("have.value", "admin123");
+    cy.get('input[name="username"]').should("be.visible").type("Admin");
+    cy.get('input[name="password"]').should("be.visible").type("admin123");
     cy.get('button[type="submit"]').should("be.visible"); //Cek apakah tombol login terlihat
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').should("be.visible").click();
 
     cy.url().should("include", "/dashboard"); // ini untuk memastikan URL mengandung '/dashboard'
-    cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should("have.text","Dashboard"); // untuk memastikan teks "Dashboard" muncul
+    cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should(
+      "have.text",
+      "Dashboard"
+    ); // untuk memastikan teks "Dashboard" muncul
   });
 
   it("TC_002 - User login menggunakan username benar dan password benar lalu login dengan tombol enter", () => {
     cy.get('input[name="username"]').should("be.visible").type("Admin");
-    cy.get('input[name="password"]').should("be.visible").type("admin123{enter}");
+    cy.get('input[name="password"]')
+      .should("be.visible")
+      .type("admin123{enter}");
 
     cy.url().should("include", "/dashboard");
-    cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should("have.text", "Dashboard");
+    cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should(
+      "have.text",
+      "Dashboard"
+    );
   });
 
   it("TC_003 - User login menggunakan username salah dan password salah", () => {
     cy.get('input[name="username"]').should("be.visible").type("AdminSalah");
     cy.get('input[name="password"]').should("be.visible").type("admin123Salah");
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').should("be.visible").click();
 
-    cy.get(".oxd-alert-content-text").should("contain.text", "Invalid credentials");
+    cy.get(".oxd-alert-content-text").should(
+      "contain.text",
+      "Invalid credentials"
+    );
   });
 
   it("TC_004 - User login menggunakan username benar dan password salah", () => {
     cy.get('input[name="username"]').should("be.visible").type("Admin");
     cy.get('input[name="password"]').should("be.visible").type("admin123Salah");
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').should("be.visible").click();
 
-    cy.get(".oxd-alert-content-text").should("contain.text", "Invalid credentials");
+    cy.get(".oxd-alert-content-text").should(
+      "contain.text",
+      "Invalid credentials"
+    );
   });
 
   it("TC_005 - User login menggunakan username salah dan password benar", () => {
     cy.get('input[name="username"]').should("be.visible").type("AdminSalah");
     cy.get('input[name="password"]').should("be.visible").type("admin123");
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').should("be.visible").click();
 
-    cy.get(".oxd-alert-content-text").should("contain.text", "Invalid credentials");
+    cy.get(".oxd-alert-content-text").should(
+      "contain.text",
+      "Invalid credentials"
+    );
   });
 
   it("TC_006 - User login dengan username kosong dan password benar", () => {
-    cy.get('input[name="password"]').type("admin123");
-    cy.get('button[type="submit"]').click();
+    cy.get('input[name="password"]').should("be.visible").type("admin123");
+    cy.get('button[type="submit"]').should("be.visible").click();
 
     // Cara validasi pesan required muncul di field username
-    cy.get(":nth-child(2) > .oxd-input-group > .oxd-text").should("be.visible").and("have.text", "Required");
+    cy.get(":nth-child(2) > .oxd-input-group > .oxd-text")
+      .should("be.visible")
+      .and("have.text", "Required");
   });
 
   it("TC_007 - User login dengan username benar dan password kosong", () => {
-    cy.get('input[name="username"]').type("Admin");
-    cy.get('button[type="submit"]').click();
+    cy.get('input[name="username"]').should("be.visible").type("Admin");
+    cy.get('button[type="submit"]').should("be.visible").click();
 
     // Cara validasi pesan required muncul di field password
-    cy.get(":nth-child(3) > .oxd-input-group > .oxd-text").should("be.visible").and("have.text", "Required");
+    cy.get(":nth-child(3) > .oxd-input-group > .oxd-text")
+      .should("be.visible")
+      .and("have.text", "Required");
   });
 
   it("TC_008 - User login dengan username kosong dan password kosong", () => {
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').should("be.visible").click();
 
     // Cara validasi pesan required muncul di fielad username dan field password
-    cy.get(":nth-child(2) > .oxd-input-group > .oxd-text").should("be.visible").and("have.text", "Required");
-    cy.get(":nth-child(3) > .oxd-input-group > .oxd-text").should("be.visible").and("have.text", "Required");
+    cy.get(":nth-child(2) > .oxd-input-group > .oxd-text")
+      .should("be.visible")
+      .and("have.text", "Required");
+    cy.get(":nth-child(3) > .oxd-input-group > .oxd-text")
+      .should("be.visible")
+      .and("have.text", "Required");
   });
 });
